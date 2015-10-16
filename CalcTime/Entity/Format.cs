8e908770dc43282336eeb207f1e3d7371e77533d
yong <kyt8724@gmail.com>
2015-10-15 22:17:42 -0400

    Deserialize : Additional File by YTKim 10/15/2015

---
+++ CalcTime/Entity/Format.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTime.Entity
+{
+    public class Format
+    {
+        public class JsonData
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+        public List<JsonData> FormatList { get; set; }
+    }
+}
